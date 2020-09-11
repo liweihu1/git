284ecfc6a47bdc74f7fe8e2d6d33c2e868cdf6d3
Li Wei Hu <liweihu12@gmail.com>
2020-09-11 16:31:24 +0200

    Add testcode

--- hello.py
+++ hello.py
@@ -1,5 +1,5 @@
-from pydriller import RepositoryMining
+from pydriller import RepositoryMining, GitRepository
 
 for commit in RepositoryMining('../git').traverse_commits():
     print(commit.author.name)
-    print(commit.branches)
+    print(GitRepository('../git').get_head().hash)
